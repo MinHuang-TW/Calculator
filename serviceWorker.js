50d59f21cb30270cbfb6e8029c1b998fccbb0b72
Min Huang <h.min719@gmail.com>
2020-06-30 15:18:43 +0200

    [serviceWorker] add service worker

---
+++ serviceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = 'v1';
+
+self.addEventListener('install', (event) => {
+  console.log('Service Worker: Installed');
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== cacheName) return caches.delete(cache);
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const appClone = response.clone();
+        caches.open(cacheName).then((cache) => {
+          cache.put(event.request, appClone);
+        });
+        return response;
+      })
+      .catch((error) =>
+        caches.match(event.request).then((response) => response)
+      )
+  );
+});
